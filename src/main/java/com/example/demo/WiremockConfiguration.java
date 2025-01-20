60fce2245309ba193731b05047443c31d0857568
Georgi Asparuhov <g.asparuhov@gmail.com>
2025-01-20 12:29:57 +0200

    first commit

---
+++ src/main/java/com/example/demo/WiremockConfiguration.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.wiremock.grpc.GrpcExtensionFactory;
+import org.wiremock.grpc.dsl.WireMockGrpcService;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.wiremock.grpc.dsl.WireMockGrpc.json;
+import static org.wiremock.grpc.dsl.WireMockGrpc.method;
+
+@Configuration
+public class WiremockConfiguration {
+
+    @Bean
+    public WireMockServer grpcWireMockServer() {
+        var server = startGrpcWiremockServer();
+
+        return server;
+    }
+
+    @Bean
+    public WireMockGrpcService marginReportHistoryServiceGrpc(@Qualifier("grpcWireMockServer") WireMockServer grpcWireMockServer) {
+        var mockService = new WireMockGrpcService(new WireMock(grpcWireMockServer.port()), "com.example.grpc.GreetingService");
+
+        System.out.println("Port: " + grpcWireMockServer.port());
+
+        mockService.stubFor(
+                method("/greeting")
+                        .withRequestMessage(equalToJson("{ \"name\":  \"Tom\" }"))
+                        .willReturn(json("{ \"greeting\": \"Hi Tom from JSON\" }"))
+        );
+
+        return mockService;
+    }
+
+    public WireMockServer startGrpcWiremockServer() {
+        var server = new WireMockServer(wireMockConfig()
+                .dynamicPort()
+                .withRootDirectory("src/main/resources/wiremock")
+                .extensions(new GrpcExtensionFactory())
+        );
+
+        server.start();
+
+        return server;
+    }
+}
